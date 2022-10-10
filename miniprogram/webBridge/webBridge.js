9d7f52a9a2c22db0ad3d9587ba5ca76381649644
cbingb666 <cbingb666@163.com>
2022-10-10 19:38:24 +0800

    fix(webBridge): 修复webBridge在真机无效

--- miniprogram/webBridge/webBridge.js
+++ miniprogram/webBridge/webBridge.js
@@ -1,69 +1,58 @@
-import { webBridgeBus } from "./webBridgeBus"
+import { webBridgeBus } from "./webBridgeBus";
 
 // webBridge/webBridge.js
 Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {},
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    wx.navigateBack({
+      success: () => {
+        webBridgeBus.$emit(
+          "webBridge",
+          options.data ? JSON.parse(options.data) : undefined
+        );
+      },
+    });
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {},
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
-        wx.navigateBack()
-        webBridgeBus.$emit('webBridge', options.data ? JSON.parse(options.data) : undefined)
-    },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {},
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {},
 
-    },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {},
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {},
 
-    },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {},
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
-    }
-})
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {},
+});
